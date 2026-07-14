c350a4d17a0cae7dcb724ceafe01b337f79bfe67
otrahigus <mdasugiharto@gmail.com>
2026-07-14 16:37:19 +0700

    memperbarui

--- utils/sheets.py
+++ utils/sheets.py
@@ -33,7 +33,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
+    "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive",
 ]
 
